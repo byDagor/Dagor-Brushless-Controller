3dcddb2dc94627a50012a931d0eb27b42a326bfa
David <davidglzrys@gmail.com>
2021-03-14 00:22:42 -0600

    New DEV branch

---
+++ Firmware/a_DA024F120/c_SETUP.ino
@@ -0,0 +1,31 @@
+
+//###########################################
+// SETUP
+//###########################################
+
+void setup() {
+  Serial.begin(115200);
+
+  //Pinmodes assignment
+  pinMode(15,OUTPUT);
+  digitalWrite(15,HIGH);
+  pinMode(so1, INPUT);
+  pinMode(so2, INPUT);
+  pinMode(so3, INPUT);
+  pinMode(nFault, INPUT);
+  pinMode(enGate, OUTPUT);
+  digitalWrite(enGate, LOW);
+
+  //SPI start up
+  pinMode(cs, OUTPUT);
+  digitalWrite(cs, HIGH);
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE1);
+
+  delay(250);
+  drv_init();
+
+  SimpleFOCinit();
+  
+}
