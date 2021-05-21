25fae3db6c061c5fbf72e76adbcec2a5e402a23f
David <davidglzrys@gmail.com>
2021-05-20 23:16:32 -0500

    Actuator gravity compensation mode

---
+++ Firmware/a_DA024F121_ACT/b_GLOBAL.ino
@@ -0,0 +1,84 @@
+
+//###########################################
+// GLOBAL TAB
+//###########################################
+
+// Libraries, pin number assignment and instance initialization
+
+//SimpleFOC Version 2.1
+#include <SimpleFOC.h>
+#include <SPI.h>
+#include <esp_now.h>
+#include <WiFi.h>
+
+//#######_THREE PHASE DRIVER - DRV8305_########
+// Datasheet: www.ti.com/lit/ds/symlink/drv8305.pdf
+#define enGate 2       //Chip Enable
+#define nFault 4       //Fault reading
+#define cs 5           //DRV8305 Chip-select
+#define so1 36
+#define so2 35
+#define so3 34
+bool faultTrig = false;
+
+//######_MAGNETIC SENSOR - AS5147_######
+// Datasheet: https://ams.com/documents/20143/36005/AS5147_DS000307_2-00.pdf
+#define sensorCS 16             //AS5147 Chip-select
+
+//######_TEMPERATURE SENSOR - STLM20_######
+// Datasheet: https://datasheet.lcsc.com/szlcsc/1810010411_STMicroelectronics-STLM20W87F_C129796.pdf
+#define vTemp 39
+
+//######_Voltage Monitor_#######
+#define vMonitor 33
+
+//#####_TIME MANAGEMENT_#####
+unsigned long runTime, prevT = 0, timeDif, stateT;
+int timeInterval = 1000, totalTempTime;
+
+//####_SIMPLEFOC INSTANCES_####
+BLDCMotor motor = BLDCMotor(pp);                                                      //BLDCMotor instance
+BLDCDriver3PWM driver = BLDCDriver3PWM(25, 26, 27);                                   //3PWM Driver instance
+MagneticSensorSPI sensor = MagneticSensorSPI(AS5147_SPI, sensorCS);                   //SPI Magnetic sensor instance
+LowsideCurrentSense current_sense = LowsideCurrentSense(0.002, 20.0, so1, so2);  //Current sensing instance
+
+//####_COMMANDER INTERFACE_####
+Commander command = Commander(Serial);
+void onMotor(char* cmd){ command.motor(&motor, cmd); }
+Commander commandEspNow = Commander();
+void onMotorEspNow(char* cmd){ commandEspNow.motor(&motor, cmd); }
+
+//######_ESPNOW_######
+typedef struct struct_message {
+    String function;
+    float value;
+} struct_message;
+
+struct_message inputData;
+struct_message outputData;
+esp_now_peer_info_t peerInfo;
+
+// Master Controller Address
+uint8_t broadcastAddress[] = {0x24, 0x6F, 0x28, 0x51, 0xED, 0xA4};
+
+//######_SETUP FUNCTIONS INIT_######
+void SimpleFOCinit();
+void drv_init();
+void spi_init();
+void gpio_init();
+void current_dc_calib(bool activate);
+
+//######_LOOP FUNCTIONS INIT_######
+void timeManagement();
+void gravityComp();
+void tempStatus(bool debug = false);
+void voltageMonitor(bool debug = false);
+void rotorPosition();
+void faultStatus();
+void printCurrents(bool dcEquivalent = false);
+
+//######_ESP_NOW_######
+void espNowInit();
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len);
+void sendData();
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
---
+++ Firmware/a_DA024F121_ACT/e_SETUP_FUNCTIONS.ino
@@ -0,0 +1,184 @@
+
+//###########################################
+// SETUP FUNCTIONS
+//###########################################
+
+
+// Initialization of SimpleFOC
+// Do NOT remove the delays in this function.
+void SimpleFOCinit(){
+  //Motor driver initialization
+  
+  _delay(500);
+
+  sensor.init();                  // Initialise magnetic sensor hardware
+  motor.linkSensor(&sensor);      // Link sensor to motor instance  
+  
+  // driver config, power supply voltage [V]
+  driver.voltage_power_supply = sourceVoltage;
+  //driver.voltage_limit = sourceVoltage;
+  driver.init();
+  motor.linkDriver(&driver);
+
+  Serial.println("DAGOR: Init current sense");
+  //current_dc_calib(true);
+  // current sense init hardware
+  current_sense.init();
+  current_sense.driverSync(&driver);
+  current_dc_calib(false);
+  
+  // link the current sense to the motor
+  if (trueTorque){
+    motor.linkCurrentSense(&current_sense);
+    motor.torque_controller = TorqueControlType::foc_current; 
+  }
+  else{
+    motor.torque_controller = TorqueControlType::voltage; 
+
+    motor.voltage_limit = phaseRes*maxPowersourceCurrent; 
+    // Measured phase resistance [ohms]
+    //motor.phase_resistance = phaseRes;
+    //motor.current_limit = maxPowersourceCurrent;
+  }
+  
+
+  if (focModulation) motor.foc_modulation = FOCModulationType::SinePWM;
+  else motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
+
+  // set FOC loop to be used: MotionControlType::torque, velocity, angle, velocity_openloop, angle_openloop
+  if (controlType == "C0") motor.controller = MotionControlType::torque;
+  else if (controlType == "C1") motor.controller = MotionControlType::velocity;
+  else motor.controller = MotionControlType::angle;
+
+  // Sensor aligning voltage
+  motor.voltage_sensor_align = (1.25*phaseRes)*alignStrength;
+
+  // Current PI controller parameters
+  motor.PID_current_q.P = cp;
+  motor.PID_current_q.I = ci;
+  motor.PID_current_q.D = cd;
+  motor.PID_current_q.limit = phaseRes*maxPowersourceCurrent;
+  motor.PID_current_q.output_ramp = voltageRamp;
+  motor.LPF_current_q.Tf = lpQDFilter;
+
+  motor.PID_current_d.P = cp;
+  motor.PID_current_d.I = ci;
+  motor.PID_current_d.D = cd;
+  motor.PID_current_d.limit = phaseRes*maxPowersourceCurrent;
+  motor.PID_current_d.output_ramp = voltageRamp;
+  motor.LPF_current_d.Tf = lpQDFilter;
+
+  /*
+  // velocity PI controller parameters
+  motor.PID_velocity.P = vp;
+  motor.PID_velocity.I = vi;
+  motor.PID_velocity.D = vd;
+  motor.PID_velocity.output_ramp = voltageRamp;
+  motor.LPF_velocity.Tf = lpVelFilter;
+  */
+  
+  // angle P controller
+  motor.P_angle.P = ap;
+  motor.P_angle.I = ai;
+  motor.P_angle.D = ad;
+  // maximal velocity of the poisition control
+  motor.velocity_limit = velocityLimit;
+  motor.LPF_angle.Tf = lpPosFilter;
+  
+  
+  // Position offset, used to define an absolute 0 position [rads]
+  motor.sensor_offset = sensorOffset;
+
+  // Downsampling value of the motion control loops with respect to torque loops
+  motor.motion_downsample = motionDownSample; // - times (default 0 - disabled)
+
+  motor.useMonitoring(Serial);      // use monitoring functionality
+  motor.init();                     // initialise motor
+  
+  if (skipCalibration && natDirection == "CW") motor.initFOC(elecOffset,CW);              // start FOC
+  else if (skipCalibration && natDirection == "CCW") motor.initFOC(elecOffset,CCW);       // start FOC
+  else motor.initFOC();                                         // align sensor/ encoder and start FOC
+  
+  _delay(500);
+
+  // define the motor id
+  command.add(motorID, onMotor, " BLDC");
+  if (!commandDebug) command.verbose = VerboseMode::nothing;
+  commandEspNow.add(motorID, onMotor, " BLDC");
+  //if (!commandDebug) commandEspNow.verbose = VerboseMode::nothing;
+
+  motor.PID_current_q.limit = phaseRes*maxPowersourceCurrent;
+  motor.PID_current_d.limit = phaseRes*maxPowersourceCurrent;
+  
+  Serial.println("DAGOR: Ready BLDC.");
+  
+}
+
+//Configure DRV8305 to desired operation mode
+void drv_init(){
+  Serial.println("DRIVER: DRV8305 INIT");
+
+  //Set to three PWM inputs mode
+  digitalWrite(cs, LOW);
+  byte resp1 = SPI.transfer(B00111010);
+  byte resp2 = SPI.transfer(B10000110);
+  digitalWrite(cs, HIGH);
+  //Serial.println(resp1, BIN);
+  //Serial.println(resp2, BIN);
+
+  //Clamp sense amplifier output to 3.3V
+  digitalWrite(cs, LOW);
+  byte resp3 = SPI.transfer(B01001100);
+  byte resp4 = SPI.transfer(B10100000);
+  digitalWrite(cs, HIGH);
+  //Serial.println(resp1, BIN);
+  //Serial.println(resp2, BIN);
+
+  Serial.println("DRIVER: enGate Enabled");
+  digitalWrite(enGate, HIGH);
+  _delay(500);
+  
+}
+
+void current_dc_calib(bool activate){
+  //Activate DC calibration mode on DRV8305
+  if (activate){
+    digitalWrite(cs, LOW);
+    byte resp5 = SPI.transfer(B01010111);
+    byte resp6 = SPI.transfer(B00000000);
+    digitalWrite(cs, HIGH);
+    //Serial.println(resp5, BIN);
+    //Serial.println(resp6, BIN);
+  }
+  //Deactivate DC calibration mode on DRV8305
+  //Set DRV83045's amplifier gain to 20x
+  else if (!activate){
+    digitalWrite(cs, LOW);
+    byte resp7 = SPI.transfer(B01010000);
+    byte resp8 = SPI.transfer(B00010101);
+    digitalWrite(cs, HIGH);
+    //Serial.println(resp7, BIN);
+    //Serial.println(resp8, BIN);
+  }
+}
+
+void spi_init(){
+  //SPI start up
+  pinMode(cs, OUTPUT);
+  digitalWrite(cs, HIGH);
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE1);
+}
+
+void gpio_init(){
+  //Pinmodes assignment
+  pinMode(15,OUTPUT);
+  digitalWrite(15,HIGH);
+  pinMode(so1, INPUT);
+  pinMode(so2, INPUT);
+  pinMode(so3, INPUT);
+  pinMode(nFault, INPUT);
+  pinMode(enGate, OUTPUT);
+  digitalWrite(enGate, LOW);
+}
